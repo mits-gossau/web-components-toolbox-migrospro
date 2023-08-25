1948759d4dc13c8f6e0463fc6f99b73f681822f9
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-08-25 14:44:47 +0200

    refactor(RegisterForm.js): remove unused CSS styles and add accent color to radio inputs
    refactor(Register.html): update radio button labels and ids for Civility field in registration form

--- src/es/components/organisms/registerForm/RegisterForm.js
+++ src/es/components/organisms/registerForm/RegisterForm.js
@@ -48,7 +48,7 @@ export default class RegisterForm extends Shadow() {
       }
       :host m-form {
         max-width: 560px;
-        margin: 0 auto;
+        margin: 0 auto 4rem auto;
       }
       :host .form-group {
         margin-bottom: 1rem;
@@ -56,35 +56,6 @@ export default class RegisterForm extends Shadow() {
       :host label {
           color: var(--m-gray-600);
           font-size: 14px;
-      }
-      :host hr {
-          color: var(--m-gray-300);
-          margin: 2rem 0;
-      }
-      :host ul {
-          list-style: none;
-          padding: 0;
-      }
-      :host li {
-          display: flex;
-          align-items: center;
-          margin-bottom: 24px;
-      }
-      :host li a-picture {
-          width: 85px;
-          height: 60px;
-      }
-      :host li p {
-          color: #9E9E9E;
-          font-size: 12px;
-          width: 75%;
-          margin: 0 0 0 12px;
-      }
-      :host .register-form {
-        margin-bottom: 4rem;
-      }
-      :host input[type="submit"] {
-          padding: 12px 24px;
       }
       :host .form-radio-group {
         display: flex;
@@ -95,6 +66,12 @@ export default class RegisterForm extends Shadow() {
         align-items: center;
         gap: 0.5rem;
       }
+      :host input[type='radio'] {
+        accent-color: var(--m-orange-700);
+      }
+      :host input[type="submit"] {
+        padding: 12px 24px;
+    }
     `
   }
 
