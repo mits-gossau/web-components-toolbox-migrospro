44c478eee25773769ee19f914a6f34369395c156
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-09-22 10:12:24 +0200

    feat(RequestForm.js): add functionality to show/hide password on click fix(RequestForm.js): fix variable naming convention for showPasswordText and hidePasswordText style(RequestForm.js): add CSS styles for host, h2, and m-form h3 elements
    style(RequestForm.js): update styles for input[type='radio'], .icon-chevron, textarea:focus, input:focus, input[type='password'] + span, input[type='text'] + span, input[type='submit'], button, label a, .form-group, .form-group > div, label, .form-text, .form-radio-group
    feat(RequestForm.js): add media query styles for larger screens
    
    chore(Benutzerprofil.html): comment out the "Mes favoris" tab in the tab navigation
    
    chore(Benutzerprofil.html): comment out unused section for favorites
    feat(Benutzerprofil.html): add form for user settings with validation errors and submit button
    
    refactor(Benutzerprofil.html): replace o-grid element with o-migrospro-request-form element

--- src/es/components/organisms/requestForm/RequestForm.js
+++ src/es/components/organisms/requestForm/RequestForm.js
@@ -13,18 +13,37 @@ import { Shadow } from "../../web-components-toolbox/src/es/components/prototype
 export default class RequestForm extends Shadow() {
   constructor(options = {}, ...args) {
     super({ importMetaUrl: import.meta.url, ...options }, ...args);
+    const showPasswordText = this.getAttribute("show-password-text") || "show";
+    const hidePasswordText = this.getAttribute("hide-password-text") || "hide";
 
-    const termsCheckbox = this.root.querySelector('input[type="checkbox"]');
-    const submitButton = this.root.querySelector('input[type="submit"]');
-
+    // terms checkbox
+    const termsCheckbox = this.root.querySelector('input[type="checkbox"]')
+    const submitButton = this.root.querySelector('input[type="submit"]')
     if (termsCheckbox && submitButton) {
       termsCheckbox.addEventListener("change", function () {
         // @ts-ignore
         if (termsCheckbox.checked) {
-          submitButton.removeAttribute("disabled");
+          submitButton.removeAttribute("disabled")
         } else {
-          submitButton.setAttribute("disabled", "true");
+          submitButton.setAttribute("disabled", "true")
         }
+      });
+    }
+
+    // show password
+    const showHidePasswords = this.root.querySelectorAll('input[type="password"] + span');
+    if (showHidePasswords) {
+      showHidePasswords.forEach(element => {
+        element.addEventListener("click", function () {
+          const input = this.previousElementSibling
+          if (input.getAttribute("type") === "password") {
+            input.setAttribute("type", "text")
+            element.innerHTML = hidePasswordText
+          } else {
+            input.setAttribute("type", "password")
+            element.innerHTML = showPasswordText
+          }
+        });
       });
     }
   }
@@ -47,6 +66,24 @@ export default class RequestForm extends Shadow() {
 
   renderCSS() {
     this.css = /* css */ `
+        :host {
+          --background-color: transparent; 
+          --font-size: 14px;
+          --color: var(--m-gray-600);
+          --color-black: var(--m-black);
+          color: var(--color, var(--m-gray-600));
+          font-size: var(--font-size);
+        }
+        :host h2,
+        :host m-form h3 {
+          color: var(--m-orange-600);
+          font-family: var(--font-family);
+          font-size: 1.25rem;
+          font-weight: 400;
+          margin: 1rem 0 .5rem 0;
+          text-align: left;
+          width: 100%;
+        }
         :host input[type="checkbox"] {
             height: 1.5rem;
             margin-right: 0.5rem;
@@ -54,13 +91,43 @@ export default class RequestForm extends Shadow() {
             position: relative;
             top: 0.375rem;
         }
-        :host input[type="submit"] {
+        :host input[type='radio'] {
+          accent-color: var(--m-orange-700);
+        }
+        :host .icon-chevron :after {
+          background-image: var(--background-image, url('_import-meta-url_../../../../icons/chevron_right.svg'));
+          background-position: center;
+          background-repeat: no-repeat;
+          background-size: 1em;
+          color: transparent;
+          min-width: 3em;
+        }
+        :host textarea:focus, 
+        :host input:focus {
+            color: var(--color-secondary);
+        }
+        :host input[type="password"] + span,
+        :host input[type="text"] + span {
+            color: var(--color);
+            cursor: pointer;
+            display: flex;
+            height: 100%;
+            justify-content: center;
+            align-items: center;
+            font-size: smaller;
+            padding: 0 1rem;
+            position: absolute;
+            right: 0;
+            top: 0;
+        }
+        :host input[type="submit"],
+        :host button {
             background-color: var(--color-secondary);
             border: var(--border-width, 1px) solid var(--border-color, var(--m-orange-600));
             border-radius: var(--border-radius);
             color: var(--m-white);
             cursor: pointer;
-            font-size: var(--font-size);
+            font-size: var(--font-size-input, var(--font-size));
             padding: 0.75em 1.5em;
         }
         :host input[type="submit"][disabled] {
@@ -68,7 +135,37 @@ export default class RequestForm extends Shadow() {
             opacity: 0.5;
         }
         :host label a {
+            color: var(--color-label-a, var(--color-black));
+        }
+        :host .form-group {
+          margin-bottom: 1rem;
+        }
+        :host .form-group > div {
+          position: relative;
+        }
+        :host label {
+          cursor: pointer;
+        }
+        :host label,
+        :host .form-text {
             color: var(--color);
+            font-size: var(--font-size-label, var(--font-size));
+        }
+        :host .form-radio-group {
+          display: flex;
+          gap: 2rem;
+        }
+        @media (min-width: 768px) {
+          :host {
+            display: flex;
+            gap: var(--gap, 2rem);
+          }
+          :host .content {
+            width: 68%;
+          }
+          :host .sidebar {
+            width: 32%;
+          }
         }
     `;
   }
