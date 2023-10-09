b5be1bc44ad0994479b81c7637e526bc01571cfc
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-10-09 11:32:52 +0200

    feat(ButtonToggle.js): add ButtonToggle component to the project fix(ButtonToggle.js): fix case of import statement for Shadow component fix(ButtonToggle.js): fix case of class name for ButtonToggle component fix(ButtonToggle.js): fix case of constructor parameter names fix(ButtonToggle.js): fix case of attribute names in constructor fix(ButtonToggle.js): fix case of method names in ButtonToggle class fix(ButtonToggle.js): fix case of variable name toggleDefaultOrder fix(ButtonToggle.js): fix case of method name shouldRenderCSS fix(ButtonToggle.js): fix case of method name renderCSS
    chore(CardHeading.js): remove import statement for Button component
    feat(Benutzerprofil.html): add two button toggle components with different configurations
    chore(web-components-toolbox): update subproject commit

---
+++ src/es/components/atoms/buttonToggle/ButtonToggle.js
@@ -0,0 +1,74 @@
+// @ts-check
+import { Shadow } from '../../web-components-toolbox/src/es/components/prototypes/Shadow.js'
+
+/**
+ * ButtonToggle
+ * An example at: src/es/components/pages/Bestelluebersicht.html
+ *
+ * @export
+ * @class ButtonToggle
+ * @type {CustomElementConstructor}
+ */
+
+export default class ButtonToggle extends Shadow() {
+  constructor (options = {}, ...args) {
+    super({ importMetaUrl: import.meta.url, ...options }, ...args)
+    this.url = this.getAttribute('fetch-url')
+    this.orderId = this.getAttribute('order-id')
+    this.isActive = this.getAttribute('is-active') === 'true'
+  }
+
+  connectedCallback () {
+    if (this.shouldRenderCSS()) this.renderCSS()
+
+    // @ts-ignore
+    let toggleDefaultOrder = self.Environment.getApiBaseUrl('migrospro').apiToggleDefaultOrder + '?orderId=' + this.orderId
+    if (toggleDefaultOrder) {
+      fetch(toggleDefaultOrder)
+        .then(response => response.json())
+        .then(data => {
+          if (data && data.response) {
+            console.log(data.response)
+          }
+        })
+        .catch(error => console.error(error))
+    }
+  }
+
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldRenderCSS () {
+    return !this.root.querySelector(
+      `:host > style[_css], ${this.tagName} > style[_css]`
+    )
+  }
+
+  renderCSS () {
+    this.css = /* css */ `
+      :host {
+        --color: ${this.isActive ? `var(--m-white)` : `var(--m-orange-600)`};
+        --background-color: ${this.isActive ? `var(--m-orange-600)` : `var(--m-white)`};
+        background-color: var(--background-color, transparent);
+        border-color: var(--border-color, transparent);
+        border-style: solid;
+        border-radius: var(--border-radius, var(--button-action-border-radius, 0.5em));
+        border-width: var(--border-width, var(--button-action-border-width, var(--button-action-border-width-custom, 2px)));
+        cursor: pointer;
+        height: var(--height, 2.5rem);
+        width: var(--width, 3.5rem);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+      :host(:hover) {
+        --color: var(--m-orange-600);
+        border-color: var(--border-color-hover, var(--m-orange-600));
+        background-color: var(--background-color-hover, transparent);
+      }
+    `
+  }
+
+}
