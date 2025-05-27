49607796d117e94bd9c86d571b4036fd95e98736
Fabienne Urscheler <fabienne.urscheler@mgb.ch>
2025-05-27 16:41:06 +0200

    custom product teaser tooltip versuche

---
+++ src/es/components/atoms/migrosproTooltip/MigrosproTooltip.js
@@ -0,0 +1,183 @@
+// @ts-check
+import Tooltip from "../../web-components-toolbox/src/es/components/atoms/tooltip/Tooltip.js"
+
+/**
+ * @export
+ * @class Tooltip
+ * @type {CustomElementConstructor}
+ */
+export default class MProTooltip extends Tooltip {
+  constructor (options = {}, ...args) {
+    super({ importMetaUrl: import.meta.url, ...options }, ...args)
+    this.clickListener = event => {
+      this.tooltipContent.classList.toggle('show')
+    }
+  }
+
+  connectedCallback () {
+    if (this.shouldRenderCSS()) this.renderCSS();
+    if (this.shouldRenderHTML()) this.renderHTML();
+    this.root.addEventListener('click', this.clickListener)
+  }
+
+  disconnectedCallback () {
+    this.root.removeEventListener('click', this.clickListener)
+  }
+
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldRenderCSS () {
+    return !this.root.querySelector(`${this.cssSelector} > style[_css]`)
+  }
+
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldRenderHTML () {
+    return !this.root.querySelector(".teaser-list");
+  }
+
+  /**
+   * renders the css
+   *
+   * @return {Promise<void>}
+  */
+  renderCSS () {
+    this.css = /* css */`
+    :host {
+      display: var(--display, block);
+      width: var(--width, auto);
+    }
+    :host .tooltip {
+      --width: ${this.tooltipWidth}em;
+      --half: ${this.tooltipWidth / -2}em;
+      display: var(--tooltip-display, inline-block);
+      position: var(--tooltip-position, relative);
+    }
+    :host .tooltip-text {
+      --text-align: ${this.textAlign ? this.textAlign : 'left'};
+      border-radius:var(--tooltip-text-border-radius, 0.25em);
+      bottom: var(--tooltip-text-bottom, 100%);
+      color: var(--tooltip-text-color, #FFFFFF);
+      background-color: var(--tooltip-background-color, var(--m-black, #333));
+      font-size: var(--tooltip-text-font-size, 0.85em);
+      left: var(--tooltip-text-left, 50%);
+      line-height: var(--tooltip-text-line-height, normal);
+      margin-left: var(--half);
+      padding: var(--tooltip-text-padding, 0.5em);
+      position: var(--tooltip-text-position, absolute);
+      text-align: var(--text-align, left);
+      visibility: var(--tooltip-text-visibility, hidden);
+      width: var(--width);
+      z-index: var(--tooltip-text-z-index, 1);
+    }
+    :host .tooltip:hover .tooltip-text {
+      background-color: var(--m-gray-800, black);
+      visibility: var(--tooltip-hover-visibility, visible);
+    }
+    :host .tooltip .tooltip-text::after {
+      border-color: var(--m-gray-800, black) transparent transparent transparent;
+      border-style: solid;
+      border-width: 0.25em;
+      content: " ";
+      left: 50%;
+      margin-left: -0.25em;
+      position: absolute;
+      top: 100%;
+    }
+    :host .tooltip-text-icon {
+      margin-bottom: var(--tooltip-text-icon-margin-bottom, 0.4em);
+    }
+    :host .icon-img {
+      height: var(--icon-img-height, 1.5em);
+      width: var(--icon-img-width, auto);
+    }
+    @media only screen and (max-width: _max-width_) {
+      :host .tooltip-text {
+        margin-left: -5px;
+      }
+      :host .tooltip:hover .tooltip-text {
+        visibility: var(--tooltip-hover-visibility-mobile, hidden);
+      }
+      :host div.show span {
+        visibility: visible !important;
+      }
+      :host .tooltip .tooltip-text::after {
+        border-color: var(--m-gray-800, black) transparent transparent transparent;
+        border-style: solid;
+        border-width: 0.25em;
+        content: " ";
+        left: 2%;
+        margin-left: -0.25em;
+        position: absolute;
+        top: 100%;
+      }
+    }
+    `
+    return this.fetchTemplate()
+  }
+
+  /**
+   * fetches the template
+   *
+   * @return {Promise<void>}
+   */
+  fetchTemplate () {
+    const styles = [
+      {
+        path: `${this.importMetaUrl}../../../../css/reset.css`, // no variables for this reason no namespace
+        namespace: false
+      },
+      {
+        path: `${this.importMetaUrl}../../../../css/style.css`, // apply namespace and fallback to allow overwriting on deeper level
+        namespaceFallback: true
+      }
+    ]
+    switch (this.getAttribute('namespace')) {
+      case 'mpro-tooltip-default-':
+        return this.fetchCSS([{
+          path: `${this.importMetaUrl}./default-/default-.css`, // apply namespace since it is specific and no fallback
+          namespace: false
+        }, ...styles], false)
+      default:
+        return this.fetchCSS(styles)
+    }
+  }
+
+  renderHTML() {
+    // path is wrong for FE, but is correct in BE!
+    const fetchModules = this.fetchModules([
+      {
+        path: "/web-components-toolbox-migrospro/src/es/components/web-components-toolbox/src/es/components/organisms/grid/Grid.js",
+        name: "o-grid",
+      },
+    ]);
+    Promise.all([fetchModules])
+  }
+
+  /**
+   * Returns the element with the class "tooltip" that is a child of the root element.
+   * @returns The method is returning the element with the class "tooltip" that is found within the root element.
+   */
+  get tooltipContent () {
+    return this.root.querySelector('.tooltip')
+  }
+
+  /**
+   * Returns the width attribute of the root element as a number, or 10 if the attribute is not present or not a valid number.
+   * @returns number|number value of the `width` attribute of the element as a number.
+   * If the `width` attribute is not present or cannot be converted to a number, it will return 10
+   */
+  get tooltipWidth () {
+    return Number(this.getAttribute('width')) || 10
+  }
+
+  get textAlign () {
+    return this.getAttribute('text-align')
+  }
+}
--- src/es/components/molecules/customProductTeaser/CustomProductTeaser.js
+++ src/es/components/molecules/customProductTeaser/CustomProductTeaser.js
@@ -22,141 +22,144 @@ export default class MProCustomProductTeaser extends Teaser {
   renderCSS() {
     this.css = /* css */ `
         :host img {
-            object-fit: cover;
-            width: 100%;
-            height: 100%;
+          object-fit: cover;
+          width: 100%;
+          height: 100%;
         }
         :host img.imageFullsize {
-            aspect-ratio: 21/16;
+          aspect-ratio: 21/16;
         }
-        :host img.imageTileview {
-            aspect-ratio: 9/10;
-            margin-bottom: var(--content-spacing, 1.143rem);
+        :host img.imageTileview, :host a-picture.imageTileview {
+          aspect-ratio: 9/10;
+          margin-bottom: var(--content-spacing, 1.143rem);
         }
         :host div.teaserContent {
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
         }
         :host h2.productName {
-            display: flex;
-            flex-wrap: wrap;
-            align-content: center;
-            font-family: var(--font-family-bold);
-            font-size: max(1.4vw, 18px);
-            line-height: var(--custom-product-teaser-h2-line-height, var(--h2-line-height, var(--line-height, 1.334em)));
-            word-break: break-all;
-            height: calc(var(--custom-product-teaser-h2-line-height, var(--h2-line-height, var(--line-height, 1.334em))) * 2);
-            margin-top: 0;
-            margin-bottom: 0;
+          display: flex;
+          flex-wrap: wrap;
+          align-content: center;
+          font-family: var(--font-family-bold);
+          font-size: max(1.4vw, 18px);
+          line-height: var(--custom-product-teaser-h2-line-height, var(--h2-line-height, var(--line-height, 1.334em)));
+          word-break: break-all;
+          height: calc(var(--custom-product-teaser-h2-line-height, var(--h2-line-height, var(--line-height, 1.334em))) * 2);
+          margin-top: 0;
+          margin-bottom: 0;
         }
         :host p {
-            font-family: var(--font-family);
-            font-size: var(--p-font-size, 1em);
-            line-height: var(--custom-product-teaser-p-line-height, var(--p-line-height, var(--line-height, 1.5)));
+          font-family: var(--font-family);
+          font-size: var(--p-font-size, 1em);
+          line-height: var(--custom-product-teaser-p-line-height, var(--p-line-height, var(--line-height, 1.5)));
         }
         :host p.articleNumber {
-            margin-top: 0;
-            margin-bottom: 1em;
+          margin-top: 0;
+          margin-bottom: 1em;
         }
         :host p.price {
-            font-size: max(1.25vw, 19px);
-            margin-top: var(--content-spacing, 1.143rem);
-            margin-bottom: var(--content-spacing, 1.143rem);
+          font-size: max(1.25vw, 19px);
+          margin-top: var(--content-spacing, 1.143rem);
+          margin-bottom: var(--content-spacing, 1.143rem);
         }
         :host div.bottomContent {
-            width: 100%;
+          width: 100%;
         }
         :host div.description {
-            display: flex;
-            align-items: flex-end;
+          display: flex;
+          align-items: flex-end;
         }
         :host div.descriptionFullsize {
-            height: calc(var(--custom-product-teaser-p-line-height, var(--p-line-height, var(--line-height, 1.5))) * var(--p-font-size) * 6);
-            margin-top: 1em;
-            margin-bottom: 1em;
+          height: calc(var(--custom-product-teaser-p-line-height, var(--p-line-height, var(--line-height, 1.5))) * var(--p-font-size) * 6);
+          margin-top: 1em;
+          margin-bottom: 1em;
         }
         :host div.descriptionTileview {
-            height: calc(var(--custom-product-teaser-p-line-height, var(--p-line-height, var(--line-height, 1.5))) * var(--p-font-size) * 3);
-            margin-top: 1em;
-            margin-bottom: 1em;
+          height: calc(var(--custom-product-teaser-p-line-height, var(--p-line-height, var(--line-height, 1.5))) * var(--p-font-size) * 3);
+          margin-top: 1em;
+          margin-bottom: 1em;
+        }
+        :host a-tooltip {
+          margin-right: 5px;
         }
         :host p.description {
-            overflow: hidden;
-            display: -webkit-box;
-            -webkit-box-orient: vertical;
+          overflow: hidden;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
         }
         :host p.descriptionFullsize {
-            -webkit-line-clamp: 6;
-            margin: 0;
+          -webkit-line-clamp: 6;
+          margin: 0;
         }
         :host p.descriptionTileview {
-            -webkit-line-clamp: 3;
-            margin: 0;
+          -webkit-line-clamp: 3;
+          margin: 0;
         }
         :host div.description span.tooltip {
-            position: absolute;
-            z-index: 1;
-            visibility: hidden;
-            font-family: var(--font-family);
-            color: var(--m-white, #ffffff);
-            background-color: var(--m-black, #333);
-            text-align: center;
-            width: auto;
-            max-width: 50%;
-            padding: 10px 8px;
-            border-radius: 0.5rem;
+          position: absolute;
+          z-index: 1;
+          visibility: hidden;
+          font-family: var(--font-family);
+          color: var(--m-white, #ffffff);
+          background-color: var(--m-black, #333);
+          text-align: center;
+          width: auto;
+          max-width: 50%;
+          padding: 10px 8px;
+          border-radius: 0.5rem;
         }
         :host div.description:hover span.tooltip {
-            visibility: visible;
+          visibility: visible;
         }
         :host a-button {
-            word-break: break-all;
-            background-color: var(--m-orange-600, #ff6600);
-            border: 0;
-            border-radius: 0.5rem;
-            margin: 0;
-            width: 100%;
+          word-break: break-all;
+          background-color: var(--m-orange-600, #ff6600);
+          border: 0;
+          border-radius: 0.5rem;
+          margin: 0;
+          width: 100%;
         }
 
         @media only screen and (max-width: _max-width_) {
             :host m-migrospro-custom-product-teaser.fullsize div.teaserContent {
-                order: 2;
+              order: 2;
             }
             :host img.imageFullsize, :host img.imageTileview {
-                aspect-ratio: 9/10;
-                margin-bottom: var(--content-spacing, 1.143rem);
+              aspect-ratio: 9/10;
+              margin-bottom: var(--content-spacing, 1.143rem);
             }
             :host div.teaserContent {
               order: 2;
             }
             :host h2.productName {
-                font-size: 2em;
-                height: auto;
+              font-size: 2em;
+              height: auto;
             }
             :host p.price {
-                font-size: 1.5em;
+              font-size: 1.5em;
             }
             :host p.descriptionFullsize, p.descriptionTileview {
-                -webkit-line-clamp: 3;
-                margin: 0;
+              -webkit-line-clamp: 3;
+              margin: 0;
             }
             :host div.description span.tooltip {
-                position: absolute;
-                left: 50%;
-                margin-left: -45%;
-                z-index: 1;
-                visibility: hidden;
-                font-family: var(--font-family);
-                color: var(--m-white, #ffffff);
-                background-color: var(--m-black, #333);
-                text-align: center;
-                width: auto;
-                max-width: 90%;
-                border-radius: 0.5rem;
+              position: absolute;
+              left: 50%;
+              margin-left: -45%;
+              z-index: 1;
+              visibility: hidden;
+              font-family: var(--font-family);
+              color: var(--m-white, #ffffff);
+              background-color: var(--m-black, #333);
+              text-align: center;
+              width: auto;
+              max-width: 90%;
+              border-radius: 0.5rem;
             }
             :host div.description:focus span.tooltip, div.description:hover span.tooltip {
-                visibility: visible;
+              visibility: visible;
             }
         }
     `;
